8a42210d7e1f8ca901d5e4a63fbdf85f6f785dcb
Thijs_serverUPC <thijs.anton.stockmans@upc.edu>
2025-04-23 17:27:25 +0200

    New names, tried to make things a bit smoother

--- test_personalfiles.py
+++ test_personalfiles.py
@@ -9,20 +9,23 @@ from rasterio.vrt import WarpedVRT
 import matplotlib.pyplot as plt
 
 def _get_point(data):
-    point = -1 
+    point = -1
     row = 0
     while point < 0 and row < data.shape[0]:
         try:
-            point = np.where(np.isfinite(data[row, :]))[0][0]
+            # point = np.where(np.isfinite(data[row, :]))[0][0]
+            point = np.where(data[row, :] > 0)[0][0]
         except:
             row += 1
 
-        if row ==200:
+        if row == 200:
             plt.imshow(data)
             plt.show()
             break
-    print(point, row)
-    return point
+    # print(point, row)
+
+    # print(data)
+    return point, row
 
 # def guess_flight_angle(data):
 #     c1 = _get_point(data.T)
@@ -47,13 +50,14 @@ def guess_flight_angle(data):
     # print(c1, d1, c2, d2)
     along_flight_angle = np.arctan2(c1 - d1_row, d1 - c1_row)
     # np.arctan2((d-d2)-c2_row, (c-c2)-d2_row)
-    along_swath_angle = np.pi - np.arctan2((c - c1) - c2_row, d2 - d2_row)
+    along_swath_angle = np.pi - np.arctan2((c - c1) - d2_row, d2 - c2_row)
     right_angle = along_flight_angle + np.pi / 2
     along_swath_diff = along_swath_angle - right_angle
-    # print(
-    #     "flight angles: ", np.rad2deg(along_flight_angle), np.rad2deg(along_swath_angle)
-    # )
-    # print("trapezoid angle: ", np.rad2deg(along_swath_diff))
+
+    print(
+        "flight angles: ", np.rad2deg(along_flight_angle), np.rad2deg(along_swath_angle)
+    )
+    print("trapezoid angle: ", np.rad2deg(along_swath_diff))
     return along_flight_angle
     #     along_swath_angle,
     #     ((c1_row, c1), (d1, d1_row), (c - c2_row, c2), (d2, d - d2_row)),
@@ -80,22 +84,23 @@ def make_fake_data():
     return src_file_source
 
 
-def orthorectify():
+def orthorectify(time="20-33-30", instrument="Telops"):
     suffix = ".tiff"
     # root_dir = "C:/Users/thijs/OneDrive/Documents/PostDoc_UPC/Data_local/Mountain_Fire_processing/day_09_04_2022/"
     # root = root_dir+'Metashape/Workswell/Metashape_data_references/Qinertia/' #Qinertia/ Interpolated/
-    root_dir = "C:/Users/thijs/OneDrive/Documents/PostDoc_UPC/Data_local/Test_Fire_processing/"
+    root_dir = "/media/Data/2022_calfide/Cedar_Creek_Fire_processing/day_09_10_2022/"
     # root = root_dir + "Metashape/Workswell/Metashape_data_references/Qinertia/"
-    root = root_dir #+ "Flir_wide/"
+    root = root_dir + f"Orthority/{instrument}/"
     # src_file_source = root_dir+'Workswell_images/22-45-29/'  # aerial image
-    src_file_source = root+'18.22.32_rot180/'  # aerial image
+    # time = "20-33-30"
+    src_file_source = root_dir+f'{instrument}_images/20.33.30/'  # aerial image
     # ext_dir = root + "single_entries210339pitch6.7/"
-    ext_dir = root + "Qinertia/single_entries182232/"
+    ext_dir = root + f"single_entries_{time}/"
     # src_files = [src_file_source + f for f in os.listdir(src_file_source) if f.endswith('.jpg')]
     # dem_file = root_dir+'USGS_mountain_fire_dem.tif'  # DEM covering imaged area
     # dem_file = root_dir+'USGS_Kaibab_Fire_dem.tif'  # DEM covering imaged area
-    dem_file = root_dir+'Merged_DSM.tif'  # DEM covering imaged area
-    int_param_file = root + "int_camera2.yaml"  # interior parameters
+    dem_file = root_dir+'USGS_Cedar_Creek_Fire_dem.tif'  # DEM covering imaged area
+    int_param_file = root_dir + "Orthority/int_camera_calfide.yaml"  # interior parameters
     # ext_param_file = root + '22-58-55_opk2.geojson'  # exterior parameters
     # ext_param_file = root + '22-45-41_opk3.csv'  # exterior parameters
     ext_param_files = [ext_dir + f for f in os.listdir(ext_dir) if f.endswith('.csv')]  # exterior parameters
@@ -117,8 +122,8 @@ def orthorectify():
         print(camera.pos)
     # create Ortho object and orthorectify
         ortho = oty.Ortho(src_file, dem_file, camera=camera, crs=cameras.crs)
-        os.makedirs(root+f"results_182232/", exist_ok=True)
-        ortho.process(root+f"results_182232/ortho_{ii}.tif", resolution=estimate_res, interp=oty.enums.Interp.nearest)
+        os.makedirs(root+f"results_{time}/", exist_ok=True)
+        ortho.process(root+f"results_{time}/ortho_{ii}.tif", resolution=estimate_res, interp=oty.enums.Interp.nearest)
 
     return 0
 
@@ -170,75 +175,86 @@ def combine_orthos(num = "1"):
         dst.write(last)
     return 0
 
-def combine_orthosv2(num = "20", stripwidth = 24):
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+
+def combine_orthosv2(num = "20", stripwidth = 24, instrument="Telops"):
     """combines the orthorectified images through the the centerlines of the overlapping pixels"""
     # root_dir = "C:/Users/thijs/OneDrive/Documents/PostDoc_UPC/Data_local/Mountain_Fire_processing/day_09_04_2022/"
     # root = root_dir+'Metashape/Workswell/Metashape_data_references/Qinertia/' #Qinertia/ Interpolated/
-    root_dir = "C:/Users/thijs/OneDrive/Documents/PostDoc_UPC/Data_local/Test_Fire_processing/"
-    root = root_dir #+ "Flir_wide/"
-    orthos_uint8 = [ii for ii in os.listdir(root+f"results_182232/") if ii.endswith('.tif') and "trimmed" not in ii]
+    root_dir = "/media/Data/2022_calfide/Cedar_Creek_Fire_processing/day_09_10_2022/"
+    root = root_dir + f"Orthority/{instrument}/"
+    time = num#"20-33-30"
+    source = root+f"results_{time}/"
+    orthos_uint8 = [ii for ii in os.listdir(source) if ii.endswith('.tif') and "trimmed" not in ii]
+    orthos_numbers = [ii.split("_")[1].rstrip(".tif") for ii in orthos_uint8]
+    if len(orthos_uint8) > 500:
+        list_of_lists = split(orthos_uint8, len(orthos_uint8)//300)
+    else: 
+        list_of_lists = [orthos_uint8]
     # orthos = [WarpedVRT(ortho, dtype="float32") for ortho in orthos_uint8]
-    orthos_trimmed = []
-    for ortho_name in orthos_uint8:
-        ortho = r_open(root+f"results_182232/{ortho_name}", "r")
-        meta = ortho.meta
-        # warp = WarpedVRT(ortho, dtype="float32")
-        data = ortho.read(1).copy()
-        # plt.imshow(data)
-        # plt.show()
-        # print("og_data: ", data[517, len(data[0])//2-12:len(data[0])//2+12])
-        along_flight_angle = guess_flight_angle(data)
-        # print(np.rad2deg(along_flight_angle))
-        mask = make_mask(data, along_flight_angle, stripwidth=stripwidth)
-        # fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-        # ax[0].imshow(np.abs(distance))
+    for number, orthos_uint8 in enumerate(list_of_lists):
+        orthos_trimmed = []
+        for ortho_name in orthos_uint8:
+            ortho = r_open(source+ortho_name, "r")
+            meta = ortho.meta
+            # warp = WarpedVRT(ortho, dtype="float32")
+            data = ortho.read(1).copy()
+            # plt.imshow(data)
+            # plt.show()
+            # print("og_data: ", data[517, len(data[0])//2-12:len(data[0])//2+12])
+            along_flight_angle = guess_flight_angle(data)
+            # print(np.rad2deg(along_flight_angle))
+            mask = make_mask(data, along_flight_angle, stripwidth=stripwidth)
+            # fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+            # ax[0].imshow(np.abs(distance))
 
-        # ax[1].imshow(data)
-        # ax[1].imshow(mask, alpha=0.5)
-        # plt.show()
-        # bla
-        # X_rot = np.clip(X_rot, 0, data.shape[0]-1).astype(int)
-        # Y_rot = np.clip(Y_rot, 0, data.shape[1]-1).astype(int)
-        # data_rot = np.zeros_like(data)
-        # data_rot[X_rot, Y_rot] = data[X, Y]
-        # data[:, len(data[0])//2+12:] = 0
-        # data[:, :len(data[0])//2-12] = 0
-        # print(data.shape, len(data[0])//2+12, len(data[0])//2-12)
-        # print(data[517, len(data[0])//2-12:len(data[0])//2+12])
-        # ortho.write(data, indexes=1)
-        data = np.where(mask, data, np.nan)
-        with rio.open(root+f"results_182232/trimmed_{ortho.name.split("/")[-1]}", 'w', **meta) as dst:
-            dst.write(data, indexes=1)
-        ortho = rio.open(root+f"results_182232/trimmed_{ortho.name.split("/")[-1]}", "r")
-        ortho_trim_warped = WarpedVRT(ortho, dtype="float32")
-        orthos_trimmed.append(ortho_trim_warped)
-    sum, outputT = merge(orthos_trimmed, method="sum", dtype="float32")
-    counts, outputT = merge(orthos_trimmed, method="count")
-    max, outputT = merge(orthos_trimmed, method="max")
-    # min, outputT = merge(orthos_trimmed, method="min")
-    # first, outputT = merge(orthos_trimmed, method="first")
-    # last, outputT = merge(orthos_trimmed, method="last")
-    # counts = counts.astype(float)
-    # sum = sum.astype(float)
-    print(sum.dtype, counts.dtype)
-    mean = np.divide(sum, counts, out=np.zeros_like(sum), where=counts!=0)
-    output_meta = ortho.meta.copy()
-    output_meta.update(
-        {   "dtype": "float32",
-            "height": mean.shape[1],
-            "width": mean.shape[2],
-            "transform": outputT,
-        }
-    )
-    # num = "21"
-    with rio.open(root+f"ortho_combined_mean_strip{num}.tif", 'w', **output_meta) as dst:
-        dst.write(mean)
-    with rio.open(root+f"ortho_combined_sum_strip{num}.tif", 'w', **output_meta) as dst:
-        dst.write(sum)
-    with rio.open(root+f"ortho_combined_counts_strip{num}.tif", 'w', **output_meta) as dst:
-        dst.write(counts)
-    with rio.open(root+f"ortho_combined_max_strip{num}.tif", 'w', **output_meta) as dst:
-        dst.write(max)
+            # ax[1].imshow(data)
+            # ax[1].imshow(mask, alpha=0.5)
+            # plt.show()
+            # bla
+            # X_rot = np.clip(X_rot, 0, data.shape[0]-1).astype(int)
+            # Y_rot = np.clip(Y_rot, 0, data.shape[1]-1).astype(int)
+            # data_rot = np.zeros_like(data)
+            # data_rot[X_rot, Y_rot] = data[X, Y]
+            # data[:, len(data[0])//2+12:] = 0
+            # data[:, :len(data[0])//2-12] = 0
+            # print(data.shape, len(data[0])//2+12, len(data[0])//2-12)
+            # print(data[517, len(data[0])//2-12:len(data[0])//2+12])
+            # ortho.write(data, indexes=1)
+            data = np.where(mask, data, np.nan)
+            with rio.open(source+f"trimmed_{ortho.name.split('/')[-1]}", 'w', **meta) as dst:
+                dst.write(data, indexes=1)
+            ortho = rio.open(source+f"trimmed_{ortho.name.split('/')[-1]}", "r")
+            ortho_trim_warped = WarpedVRT(ortho, dtype="float32")
+            orthos_trimmed.append(ortho_trim_warped)
+        sum, outputT = merge(orthos_trimmed, method="sum", dtype="float32")
+        counts, outputT = merge(orthos_trimmed, method="count")
+        max, outputT = merge(orthos_trimmed, method="max")
+        # min, outputT = merge(orthos_trimmed, method="min")
+        # first, outputT = merge(orthos_trimmed, method="first")
+        # last, outputT = merge(orthos_trimmed, method="last")
+        # counts = counts.astype(float)
+        # sum = sum.astype(float)
+        print(sum.dtype, counts.dtype)
+        mean = np.divide(sum, counts, out=np.zeros_like(sum), where=counts!=0)
+        output_meta = ortho.meta.copy()
+        output_meta.update(
+            {   "dtype": "float32",
+                "height": mean.shape[1],
+                "width": mean.shape[2],
+                "transform": outputT,
+            }
+        )
+        with rio.open(root+f"ortho_combined_mean_strip{num}_{number}.tif", 'w', **output_meta) as dst:
+            dst.write(mean)
+        with rio.open(root+f"ortho_combined_sum_strip{num}_{number}.tif", 'w', **output_meta) as dst:
+            dst.write(sum)
+        with rio.open(root+f"ortho_combined_counts_strip{num}_{number}.tif", 'w', **output_meta) as dst:
+            dst.write(counts)
+        with rio.open(root+f"ortho_combined_max_strip{num}_{number}.tif", 'w', **output_meta) as dst:
+            dst.write(max)
     return 0
     
 def main():
@@ -264,10 +280,11 @@ def main():
 #     print(result.profile)
 #     print(np.sum(result.read(1)>0))
 #     print(640*514)
-    # orthorectify()
-    num = "182232_v2"
+    num = "20-33-30"
+    orthorectify(time = num, instrument="Telops")
+    
     # combine_orthos()
-    combine_orthosv2(num=num, stripwidth=30)
+    combine_orthosv2(num=num, stripwidth=90, instrument="Telops")
     return 0
 
 if __name__ == "__main__":
